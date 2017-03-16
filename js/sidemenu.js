b65e8a7d747795f5c27dd1a5b1330ecd54619a4f
yotsuba1022 <yotsuba1022@gmail.com>
2017-03-16 13:44:22 +0800

    Apply animate.css to perform animation.

--- js/sidemenu.js
+++ js/sidemenu.js
@@ -1,5 +1,12 @@
 jQuery(document).ready(function($) {
-	$('.header').click(function(event) {
-		$('body').toggleClass('open');
-	});
+    $('.header').click(function(event) {
+        $('body').toggleClass('open');
+    });
+
+    $('.shake').click(function(event) {
+        $('.top').addClass('shake');
+        setTimeout(function(){
+        	$('.top').removeClass('shake');
+        }, 1000);
+    });
 });
