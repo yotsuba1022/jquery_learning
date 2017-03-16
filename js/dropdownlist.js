20b31a8ba6b2fbe58cce73cef7749cad9660b507
yotsuba1022 <yotsuba1022@gmail.com>
2017-03-16 11:58:41 +0800

    Add button for adjust font size.

--- js/dropdownlist.js
+++ js/dropdownlist.js
@@ -1,16 +1,28 @@
 jQuery(document).ready(function($) {
-	$('.dropdown').click(function(event) {
-		event.preventDefault();
-		$('.dropdown').toggleClass('active');
-		$('.dropdown-open').slideToggle();
-	});
+    $('.dropdown').click(function(event) {
+        event.preventDefault();
+        $('.dropdown').toggleClass('active');
+        $('.dropdown-open').slideToggle();
+    });
 
-	$('.showbox').click(function(event) {
-		$('.box1').delay(0).slideDown(1000, function() {
-		});
-		$('.box2').delay(1000).slideDown(1000, function() {
-		});
-		$('.box3').delay(2000).slideDown(1000, function() {
-		});
-	});
+    $('.showbox').click(function(event) {
+        $('.box1').delay(0).slideDown(1000, function() {});
+        $('.box2').delay(1000).slideDown(1000, function() {});
+        $('.box3').delay(2000).slideDown(1000, function() {});
+    });
+
+    $('.font-b').click(function(event) {
+        event.preventDefault();
+        $('.textblock p').css('font-size', '30px');
+    });
+
+    $('.font-n').click(function(event) {
+        event.preventDefault();
+        $('.textblock p').css('font-size', '20px');
+    });
+
+    $('.font-s').click(function(event) {
+        event.preventDefault();
+        $('.textblock p').css('font-size', '10px');
+    });
 });
