d3c3fdede0b81b4d7bdf95925a72f21d75068e7f
yotsuba1022 <yotsuba1022@gmail.com>
2017-03-16 11:25:07 +0800

    Add delay for animations.

--- js/dropdownlist.js
+++ js/dropdownlist.js
@@ -4,4 +4,13 @@ jQuery(document).ready(function($) {
 		$('.dropdown').toggleClass('active');
 		$('.dropdown-open').slideToggle();
 	});
+
+	$('.showbox').click(function(event) {
+		$('.box1').delay(0).slideDown(1000, function() {
+		});
+		$('.box2').delay(1000).slideDown(1000, function() {
+		});
+		$('.box3').delay(2000).slideDown(1000, function() {
+		});
+	});
 });
