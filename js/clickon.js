efc66fde9398ed9420136f01553ced7262159972
yotsuba1022 <yotsuba1022@gmail.com>
2017-03-30 14:19:59 +0800

    Click v.s. On

---
+++ js/clickon.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function($) {
+	$('.wrap').on('click', 'h1', function(event) {
+		event.preventDefault();
+		alert('It works');
+	});
+	// $('h1').click(function(event) {
+	// 	alert('It works.');
+	// });
+	$('.box1').html('<h1>用jQuery動態產生的</h1>');
+});
