9880f9176fbdd8334d745ced092e97370cff5db8
yotsuba1022 <yotsuba1022@gmail.com>
2017-03-31 11:39:53 +0800

    Example for delete DOM element with jQuery.

--- js/clickon.js
+++ js/clickon.js
@@ -2,6 +2,7 @@ jQuery(document).ready(function($) {
 	$('.wrap').on('click', 'h1', function(event) {
 		event.preventDefault();
 		alert('It works');
+		$('.box').remove();
 	});
 	// $('h1').click(function(event) {
 	// 	alert('It works.');
@@ -9,4 +10,8 @@ jQuery(document).ready(function($) {
 	$('.box1').html('<h1>用jQuery動態產生的</h1>');
 
 	$('img').attr('width','80');
+	
+	$('.remove').click(function(event) {
+		$(this).parent().parent().remove();
+	});
 });
